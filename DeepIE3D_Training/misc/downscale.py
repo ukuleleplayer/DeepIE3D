8b503404b95dbf6229986833c86d05c72d8ec9ec
Simonkmunck <skra@itu.dk>
2019-05-17 12:57:32 +0200

    Repository initialized. Ready to use

--- DeepIE3D_Training/misc/downscale.py
+++ DeepIE3D_Training/misc/downscale.py
@@ -11,6 +11,9 @@ global scaled_array
 
 
 def scale_n_times(input_path, output_path, n, s):
+    """
+    Concurrently scale a voxel 3D model n times, halving the dimensions each time.
+    """
     size = s
     global pool
     pool = ThreadPool(128)
@@ -32,6 +35,9 @@ def scale_n_times(input_path, output_path, n, s):
 
 
 def scale_down():
+    """
+    scale a voxel 3D model one times, halving the dimensions.
+    """
     loop_length = len(voxels)
     scaled_array = np.zeros((loop_length//2, loop_length//2, loop_length//2))
     for x in range(0, loop_length, 2):
@@ -41,6 +47,9 @@ def scale_down():
 
 
 def count_majority(x, y, z):
+    """
+    Set a voxel at x,y,z if halv or more of the 2*2*2 cube that it is scaled down from has voxels.
+    """
     v0 = voxels[x][y][z]
     v1 = voxels[x+1][y][z]
     v2 = voxels[x][y+1][z]
@@ -49,5 +58,4 @@ def count_majority(x, y, z):
     v5 = voxels[x+1][y][z+1]
     v6 = voxels[x][y+1][z+1]
     v7 = voxels[x+1][y+1][z+1]
-    scaled_array[x//2][y//2][z //
-                             2] = int(list([v0, v1, v2, v3, v4, v5, v6, v7]).count(True) >= 4)
+    scaled_array[x//2][y//2][z//2] = int(list([v0, v1, v2, v3, v4, v5, v6, v7]).count(True) >= 4)
